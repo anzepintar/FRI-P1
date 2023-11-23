44dc76af2e2d82b6b150c34bccc7f8b8b5a82b08
Anže Pintar <anze@anzepintar.com>
2023-11-23 10:09:50 +0100

    vaje t07

---
+++ t07/vaje/Volitve.java
@@ -0,0 +1,54 @@
+public class Volitve {
+
+    public static int steviloGlasov(int[][][] t, int leto, int stranka) {
+        int sum = 0;
+        for (int i = 0; i < t[leto][stranka].length; i++) {
+            sum += t[leto][stranka][i];
+        }
+        return sum;
+    }
+
+    public static int[][] glasovi(int[][][] t) {
+        int leta = t.length;
+        int stranke = t[0].length;
+        int[][] tab = new int[leta][stranke];
+        for (int i = 0; i < leta; i++) {
+            for (int j = 0; j < stranke; j++) {
+                tab[i][j] = steviloGlasov(t, i, j);
+            }
+        }
+        return tab;
+    }
+
+    public static int najVolisce(int[][][] t, int stranka) {
+        int najVolisce = 0;
+        int sumNajVolisce = t[0][stranka][0];
+        for (int i = 0; i < t[0][stranka].length; i++) {
+            int sumVolisce = 0;
+            for (int j = 0; j < t.length; j++) {
+                sumVolisce += t[j][stranka][i];
+            }
+            if (sumVolisce > sumNajVolisce) {
+                sumNajVolisce = sumVolisce;
+                najVolisce = i;
+            }
+        }
+        return najVolisce;
+    }
+
+    public static int vsotaUvrstitev(int[][][] t, int stranka, int volisce) {
+        int vsotaUvrstitev = 0;
+        for (int i = 0; i < t.length; i++) {
+            int mesto = 1;
+            int stGlasov = t[i][stranka][volisce];
+            for (int j = 0; j < t[0].length; j++) {
+                if (t[i][j][volisce] > stGlasov) {
+                    mesto++;
+                }
+            }
+            vsotaUvrstitev += mesto;
+        }
+        return vsotaUvrstitev;
+    }
+
+}
